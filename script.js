182ca70e1991b254b09e23a5e15b044dc416bbbb
Yoko <73258869+y0k0r@users.noreply.github.com>
2025-04-02 19:49:38 -0400

    feat: update UI to display results and track score

--- script.js
+++ script.js
@@ -48,48 +48,21 @@ function playRound(computerChoice, humanChoice) {
   // accept input for both computer and human choices
   // compare input and return a round winner
   if (computerChoice === humanChoice){
-    console.log("Great minds think alike! Replaying the round...")
+    results.textContent = "Great minds think alike! Replaying the round..."
   } else if ((computerChoice === "rock" && humanChoice === "paper") || (computerChoice === "paper" && humanChoice === "scissors") || (computerChoice === "scissors" && humanChoice === "rock")){
-    console.log(`You won! ${humanChoice} beats ${computerChoice}.`)
-    return "human";
+    results.textContent = `You won! ${humanChoice} beats ${computerChoice}.`
+    humanScore++;
   } else {
-    console.log(`You lost! ${computerChoice} beats ${humanChoice}.`)
-    return "computer"; 
-  }
-}
+    results.textContent = `You lost! ${computerChoice} beats ${humanChoice}.`
+    computerScore++;
 
-function playGame() {
+  }    
+  score.textContent = `Computer: ${computerScore}   Human: ${humanScore}`
   
-  let humanScore = 0;
-  let computerScore = 0;
-  let round = 1;
-
-  // Run 5 rounds of the game
-  while (round <=5 ){
-    console.log(`Round ${round}, fight!`);
-    let winner = playRound(getComputerChoice(), getHumanChoice());
-  // Increment winner of round  
-    switch (winner) {
-      case "human":
-        humanScore++;
-        round++;
-        break;
-      case "computer":
-        computerScore++;
-        round++;
-        break;
-      default:
-        break;
-    }
-  }
-  // Print overall winner of the game
-  if (humanScore > computerScore){
-    console.log(`You are the winner! [Human: ${humanScore} points | Computer: ${computerScore} points]`); } else if (computerScore > humanScore) {
-      console.log(`You lose :( [Human: ${humanScore} points | Computer: ${computerScore} points]`)
-    }
-  else {
-    console.log(`Tie game.`)
+  if (computerScore === 5) {
+    winner.textContent = 'You lost. Better luck next time!';
+  } else if (humanScore === 5) {
+    winner.textContent = 'You won!';
   }
 }
 
-playGame();
